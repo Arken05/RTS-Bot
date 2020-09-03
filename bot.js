3fc4155c280183cb1d0d780007cde25f31fc18ed
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-03 20:11:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,11 @@ client.once('ready', () => {
 
 
 client.on('message', message => {
+if (!message.content.startsWith(prefix) || message.author.bot) return;
 
+const args = message.content.slice(prefix.length).trim().split(' ');
+const command = args.shift().toLowerCase();
+ 
     if (message.content === `${prefix}ping`) {
      message.channel.send("pong");
     }
