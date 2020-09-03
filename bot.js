9b8284da9474798b81444058472e2a96ad700ff1
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-03 20:58:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,17 +28,12 @@ client.on('message', message => {
 	if (command === 'ping') {
 	 client.commands.get('ping').execute(message, args);
 	}
-    	if (command === 'server') {
+    	else if (command === 'server') {
 	   client.commands.get('server').execute(message, args);
 	}
 	else if (command === 'kick') {
-	     if (!message.mentions.users.size) {
-		      return message.reply('you need to tag a user in order to kick them!');
-	     }
-	     const taggedUser = message.mentions.users.first();
-
-	     message.channel.send(`You wanted to kick: ${taggedUser.username}`);
-	    }
+	  client.commands.get('kick').execute(message, args);
+	}
 });
 
  
