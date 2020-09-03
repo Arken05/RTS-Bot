d37c2e7e31a5c1cffb8ea2e88c7360f1991afa2c
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-03 20:50:00 +0100

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -2,4 +2,4 @@ module.exports = {
   name: 'server',
   description: 'server stats',
   execute{message, args) {
-    
+    message.channel.send(`Server Name: ${message.guild.name}\nTotal Members: ${message.guild.memberCount}`)
