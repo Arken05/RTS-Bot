a14abc39602245d8278b4bfddd88e245119df944
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-03 21:39:24 +0100

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -2,7 +2,17 @@ module.exports = {
     name: 'kick',
     description: 'to kick a user',
     execute(message, args) {
-        const member = <message>,mentions.members.first();
-	member.kick();
+        const member = message,mentions.members.first();
+	if(!member)
+		return message.reply("You need to mention a vaild user.");
+	if(!member.kickable)
+		return message.reply("I refuse.");
+	let reason = args.slice(1).join(' ');
+    	if(!reason) reason = "No reason provided";
+    
+    	await member.kick(reason)
+      		.catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+    	message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+
     }
 };
