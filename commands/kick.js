0b59c2103407c2e4a273dcd0ace081f3b73af26f
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-03 21:26:47 +0100

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -2,11 +2,7 @@ module.exports = {
     name: 'kick',
     description: 'to kick a user',
     execute(message, args) {
-        if (!message.mentions.users.size) {
-		      return message.reply('you need to tag a user in order to kick them!');
-	     }
-	     const taggedUser = message.mentions.users.first();
-
-	     message.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    },
+        const member = <message>,mentions.members.first();
+	member.kick();
+    }
 };
