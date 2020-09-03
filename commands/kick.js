cbb24a53978b609d5da71536052f7d6966fd7d3a
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-03 21:41:10 +0100

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -10,7 +10,7 @@ module.exports = {
 	let reason = args.slice(1).join(' ');
     	if(!reason) reason = "No reason provided";
     
-    	await member.kick(reason)
+    	member.kick(reason)
       		.catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
     	message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
 
