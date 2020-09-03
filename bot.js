9171916617376be7836ccb526cdda0036d1231a3
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-03 21:20:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 
-const prefix = "//";
+const prefix = require('./config.json');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
