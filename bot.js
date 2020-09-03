312450e1b6ae3a465b3c177d506fd5eea4a677c9
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-03 20:24:16 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,7 +31,7 @@ const command = args.shift().toLowerCase();
      }
      const taggedUser = message.mentions.users.first();
      
-     message.channel.send(`You wanted to kick: $taggedUser.username}`);
+     message.channel.send(`You wanted to kick: {$taggedUser.username}`);
     }
 });
 
