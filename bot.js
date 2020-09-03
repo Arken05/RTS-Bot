0549f8a6367ae06f3e4a13818d7763a0c51d63d1
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-03 20:22:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,6 +26,9 @@ const command = args.shift().toLowerCase();
      message.channel.send(`Server Name: ${message.guild.name}\nTotal Members: ${message.guild.memberCount}`)
                           }
     else if (command === 'kick') {
+     if (!message.mentions.users.size) {
+	      return message.reply('you need to tag a user in order to kick them!');
+     }
      const taggedUser = message.mentions.users.first();
      
      message.channel.send(`You wanted to kick: $taggedUser.username}`);
