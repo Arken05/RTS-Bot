fa8321ca1e8fac1b87f7d2f8a9a07d7591b7c544
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-04 18:29:20 +0100

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,1 +1,18 @@
+module.exports = {
+    name: 'ban',
+    description: 'to ban a user',
+    execute(message, args) {
+        const member = message.mentions.members.first();
+	if(!member)
+		return message.reply("```//ban <member> [reason for kick] \n \nPinging/tagging a user is a required argument. A reason is optional.```");
+	if(!member.kickable)
+		return message.reply("I refuse.");
+	let reason = args.slice(1).join(' ');
+    	if(!reason) reason = "No reason provided";
+    
+    	member.ban(reason)
+      		.catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+    	message.reply(`Banned ${member.user.tag} because: ${reason}`);
 
+    }
+};
