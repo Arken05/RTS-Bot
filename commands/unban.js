ee2167f3e5f6d1241efe45a30db6cd2f1a1302cb
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-04 18:52:35 +0100

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -2,15 +2,13 @@ module.exports = {
         name: "unban",
         description: "unban a member",
         execute(message, args) {
-                const member = message.mentions.members.first();
-                if(!member)
+                let id = args.slice(0)
+                if(!id)
                         return message.reply("```//unban <member> [reason for unban] \n \nPinging/tagging a user is a required argument. A reason is optional.```");
-                if(!member.bannable)
-                        return message.reply("I refuse.");
                 let reason = args.slice(1).join(' ');
                 if(!reason) reason = "No reason provided";
 
-                member.ban(reason)
+                id.unban(reason)
                         .catch(error => message.reply(`Sorry ${message.author} I couldn't unban because of : ${error}`));
                 message.reply(`Unbanned ${member.user.tag} because: ${reason}`);
         }
