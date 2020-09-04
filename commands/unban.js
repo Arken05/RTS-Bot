827ad76fcf511432bec473ba680a4cbbb613d415
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-04 18:44:47 +0100

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -1,3 +1,8 @@
 module.exports = {
         name: "unban",
         description: "unban a member",
+        execute(message, args) {
+                let id = args.slice(0).join(' ');
+                msg.guild.members.unban(id)
+        }
+};
