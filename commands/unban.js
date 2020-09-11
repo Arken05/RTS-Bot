84c44b1da7d20708ad1f9e4dca1ec363ac283592
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-11 20:30:46 +0100

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -10,14 +10,14 @@ if (!Reason) Reason = `Unbanned by ${message.author.tag}`
 message.guild.fetchBans()
 .then(bans => {
 if (bans.some(u => User.includes(u.username))) {
-let user = bans.find(user => user.username === User);
+  let user = bans.find(user => user.username === User);
 
-message.guild.unban(user.id, Reason);
-}
+  message.guild.unban(user.id, Reason);
+  }
 else if (bans.some(u => User.includes(u.id))) {
 
-message.guild.unban(User, Reason);
-}
+  message.guild.unban(User, Reason);
+  }
 else {
 return message.reply(`This person is not banned`);
 }
