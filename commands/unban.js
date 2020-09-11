ad3961bcc9e7e182eba1facaf4098f789a2be191
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-11 20:24:04 +0100

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -1,15 +1,31 @@
 module.exports = {
         name: "unban",
         description: "unban a member",
-        execute(message, args) {
-                let id = args.slice(0)
-                if(!id)
-                        return message.reply("```//unban <member> [reason for unban] \n \nPinging/tagging a user is a required argument. A reason is optional.```");
-                let reason = args.slice(1).join(' ');
-                if(!reason) reason = "No reason provided";
-
-                msg.guild.members.unban(id)
-                        .catch(error => message.reply(`Sorry ${message.author} I couldn't unban because of : ${error}`));
-                message.reply(`Unbanned ${id} because: ${reason}`);
+        
+        async run (client, message, args) {
+    
+            if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('You can\'t use that!')
+            if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('I don\'t have the permissions.')
+    
+            const member = message.mentions.members.first();
+    
+            if(!args[0]) return message.channel.send('Please specify a user');
+    
+    
+    
+            
+    
+            let reason = args.slice(1).join(" ");
+    
+            if(!reason) reason = 'Unspecified';
+    
+            message.guild.members.unban(`${member}`, `${reason}`)
+            .catch(err => {
+                if(err) return message.channel.send('Something went wrong')
+            })
+    
+            message.channel.send('banned');
+    
+    
         }
-};
+    }
