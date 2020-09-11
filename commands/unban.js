14390de9c6f2178f4ae8e0d6b6a3fc5d0fc79937
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-11 20:33:37 +0100

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -1,25 +1,27 @@
 const Discord = require(`discord.js`);
 
 module.exports.run = async(client, message, args) => {
+  name: 'ban',
+  description: 'to ban a user',
+    
+  let User = args[0];
+  let Reason = args.slice(1).join(` `);
+  if (!User) return message.reply(`Who are we unbanning?`);
+  if (!Reason) Reason = `Unbanned by ${message.author.tag}`
 
-let User = args[0];
-let Reason = args.slice(1).join(` `);
-if (!User) return message.reply(`Who are we unbanning?`);
-if (!Reason) Reason = `Unbanned by ${message.author.tag}`
+  message.guild.fetchBans()
+  .then(bans => {
+  if (bans.some(u => User.includes(u.username))) {
+    let user = bans.find(user => user.username === User);
 
-message.guild.fetchBans()
-.then(bans => {
-if (bans.some(u => User.includes(u.username))) {
-  let user = bans.find(user => user.username === User);
+    message.guild.unban(user.id, Reason);
+    }
+  else if (bans.some(u => User.includes(u.id))) {
 
-  message.guild.unban(user.id, Reason);
+    message.guild.unban(User, Reason);
+    }
+  else {
+  return message.reply(`This person is not banned`);
   }
-else if (bans.some(u => User.includes(u.id))) {
-
-  message.guild.unban(User, Reason);
+  }); 
   }
-else {
-return message.reply(`This person is not banned`);
-}
-});
-}
