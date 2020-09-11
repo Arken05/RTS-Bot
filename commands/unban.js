58808756e78ce46c701c5bb0d6b5c2ba9e1caab1
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-11 20:26:14 +0100

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -7,7 +7,7 @@ module.exports = {
             if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('You can\'t use that!')
             if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('I don\'t have the permissions.')
     
-            const member = message.mentions.members.first();
+            const id = args.slice(0)
     
             if(!args[0]) return message.channel.send('Please specify a user');
     
@@ -19,7 +19,7 @@ module.exports = {
     
             if(!reason) reason = 'Unspecified';
     
-            message.guild.members.unban(`${member}`, `${reason}`)
+            message.guild.members.unban(id)
             .catch(err => {
                 if(err) return message.channel.send('Something went wrong')
             })
