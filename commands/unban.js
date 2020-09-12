53b16661be21b83f81ae767b951db4f3aeed686a
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-12 09:09:27 +0100

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -3,16 +3,4 @@ const Discord = require(`discord.js`);
 module.exports.run {
   name: 'unban',
   description 'to unban a user',
-    
-  async run({ message, args, server }, t) {
-    
-    let member = await message.guild.fetchBans()
-    let ban
-    ban = member.find(b => b.user.username === args[0]) || member.get(args[0].replace(/[!@<>]/g, "")) || member.find(b => b.user.tag === args[0])
-    if (!ban) return message.reply('no')
-    }
-  
-    message.guild.members.unban(ban.user.id).then((user) => {
-      }
-    })
-}
+  execute
