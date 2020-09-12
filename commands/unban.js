23122b23acec7bdd0363b930866bbabf0f2abdbe
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-12 20:14:47 +0100

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -2,15 +2,11 @@ module.exports = {
   name: 'unban',
   description: 'to unban a user',
   execute(message, args) {
-     let id = args.slice(0)
-     message.reply("test")
-     if(!id)
-       return message.reply("```//unban <member> [reason for unban] \n \nUser ID is a required argument. A reason is optional.```")
-     let reason = args.slice(1).join(' ');
-     if(!reason) reason = "No reason provided";
-    
-     id.unban( reason )
-          .catch(error => message.reply(`Sorry ${message.author} I couldn't unban because of : ${error}`));
-    message.reply(`Unbanned ${id} beacuse: ${reason}`);
+    let userID = args[0]
+      msg.guild.fetchBans().then(bans=> {
+      if(bans.size == 0) return 
+      let bUser = bans.find(b => b.user.id == userID)
+      if(!bUser) return
+      msg.guild.members.unban(bUser.user)
   }
 };
