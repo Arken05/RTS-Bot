4d3e098cffc8e33c0a270f93e30b2f130350a25e
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-12 18:08:52 +0100

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -1,6 +1,15 @@
-const Discord = require(`discord.js`);
-
-module.exports.run {
+module.exports.run = {
   name: 'unban',
   description: 'to unban a user',
-  execute
+  execute(message, args) {
+     let id = args.slice(0)
+     if(!id)
+       return message.reply("```//unban <member> [reason for unban] \n \nUser ID is a required argument. A reason is optional.```")
+     let reasonn = args.slice(1).join(' ');
+     if(!reason) reason = "No reason provided";
+    
+     id.unban(reason)
+          .catch(error => message.reply(`Sorry ${message.author} I couldn't unban because of : ${error}`));
+    message.reply(`Unbanned ${id} beacuse: ${reason}`);
+  }
+};
