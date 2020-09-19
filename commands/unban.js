322716b433c4e867167fc752354e31701fb3d868
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-19 17:54:28 +0100

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -10,8 +10,8 @@ module.exports = {
     if(!id)
        return message.reply("```//unban <member> [reason for unban] \n \nPinging/tagging a user is a required argument. A reason is optional.```");
     let reason = args[1]
-    if(!reason) reason = "No reason"
+    if(!reason) reason = "no reason"
     message.guild.members.unban(id)
-    message.reply(`Unbanned ${id} , because ${reason}`);
+    message.reply(`Unbanned: ${id} \nReason: ${reason}`);
   }
 };
