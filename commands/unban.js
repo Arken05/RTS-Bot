5f240b320923d51f768b449a9a919519059f4e3e
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-09-19 18:21:44 +0100

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -5,8 +5,6 @@ module.exports = {
 
     //if(!message.member.hasPermission("BAN_MEMBERS") )
        //return message.reply("You don't have the permissions to do that");
-
-       return message.reply("```//unban <member> [reason for unban] \n \nPinging/tagging a user is a required argument. A reason is optional.```");
     let reason = args[1]
     if(!reason) reason = "no reason"
     let userID = args[0]
@@ -15,6 +13,5 @@ module.exports = {
       let bUser = bans.find(b => b.user.id == userID)
       if(!bUser) return
       msg.guild.members.unban(bUser.user)
-    }
   }
 };
