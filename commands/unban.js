bee7964ded2908bed3ffbb020eec851a4da50dfc
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-11-02 19:50:11 +0000

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -2,15 +2,18 @@ module.exports = {
   name: 'unban',
   description: 'to unban a user',
   execute(message, args) {
-    //if(!message.member.hasPermission("BAN_MEMBERS") )
-       //return message.reply("You don't have the permissions to do that");
+    if(!message.member.hasPermission("BAN_MEMBERS") )
+       return message.reply("You don't have the permissions to do that.");
     let reason = args[1]
     if(!reason) reason = "no reason"
     let userID = args[0]
+    if(!userID)
+      return message.reply("Must ping a user.")
     message.guild.fetchBans().then(bans=> {
     if(bans.size == 0) return 
     let bUser = bans.find(b => b.user.id == userID)
-    if(!bUser) return
+    if(!bUser)
+      return message.reply("This user is not banned.")
     message.guild.members.unban(bUser.user)
     })
   }
