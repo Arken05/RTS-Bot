b2623f3f7530089ae5174a8c7128845cee541557
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-11-03 20:51:46 +0000

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -8,7 +8,7 @@ module.exports = {
     if(!reason) reason = "no reason"
     let userID = args[0]
     if(!userID)
-      return message.reply("Must ping a user.")
+      return message.reply("```//unban <member> [reason for unban] \n \nPinging/tagging a user is a required argument. A reason is optional.```")
     //message.guild.fetchBans().then(bans=> {
     //if(bans.size == 0) return 
     //let bUser = bans.find(b => b.user.id == userID)
