0488606ac3f09afb46bbf9713be8b0c30fb3af73
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-11-03 20:36:11 +0000

    Delete purge.js

--- commands/purge.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  name: 'purge',
-  description: 'mass delete messages',
-  execute(message, args) {
-    if(!message.member.hasPermission("MANAGE MESSAGES") )
-		  return message.reply("Requires permission: MANAGE MESSAGES");
