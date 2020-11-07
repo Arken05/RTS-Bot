b54a816b3d86c141cb61dd3a7a38bc3bad762fbc
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-11-07 17:36:26 +0000

    Create mute.js

---
+++ commands/mute.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'mute',
+    description: 'mute a user for an indefinite or set amount of time',
+    execute(message, args) {
+      message.reply("This command is still under construction.")
+    }
+};
