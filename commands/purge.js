1804710a739afb8b7ba8ff34f852f6579c825d9c
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-11-07 17:32:57 +0000

    Create purge.js

---
+++ commands/purge.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'purge',
+    description: 'mass delete messages',
+    execute(message, args) {
+      message.reply("This command is still under construction.")
+    }
+};
