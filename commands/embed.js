29a599dadb5ccff42bb23186306b883f5e080792
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-11-08 14:23:06 +0000

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -17,6 +17,7 @@ module.exports = {
         {name: 'Advertising Rules', value: '1. Post in the correct channels \n\
 2. NO DM ADVERTISING' },
       )
+      .setThumbnail('https://media.discordapp.net/attachments/753667854800519223/774995173435637790/20201101_204742.jpg?width=654&height=654')
       .setFooter('')
     message.channel.send(embed);
   },
