3e420fc681946e4796df20b681f14639d0579076
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-11-08 15:09:14 +0000

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -12,10 +12,10 @@ module.exports = {
       .setTitle('RULES')
       .setDescription('Follow all the rules in order to maintain membership of the server: ')
       .addFields(
-        {name: '**Server Rules**', value: '1. Be respectful \n\
-2. Be nice' },
-        {name: 'Advertising Rules', value: '1. Post in the correct channels \n\
-2. NO DM ADVERTISING' },
+        {name: '__Server Rules__', value: ' • Be respectful \n\
+ • Be nice' },
+        {name: '__Advertising Rules__', value: ' • Post in the correct channels \n\
+ • NO DM ADVERTISING' },
       )
       .setThumbnail('https://media.discordapp.net/attachments/753667854800519223/774995173435637790/20201101_204742.jpg?width=654&height=654')
       .setFooter('')
