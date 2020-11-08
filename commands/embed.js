7ebd30cd67c145518cd7654a1e99045da7e9cdee
Arken05 <69765819+Arken05@users.noreply.github.com>
2020-11-08 13:15:50 +0000

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -1,1 +1,15 @@
-
+module.exports = {
+  name: 'embed',
+  description: 'custom embed',
+  execute(message, args) {
+    if(!message.member.hasPermission("ADMINISTRATOR") )
+      return message.reply("admin only")
+    
+    const embed = new Discord.MessageEmbed():
+      .setColor('')
+      .setTitle('EMBED')
+      .setDescription('test')
+      .setFooter('test')
+    message.channel.send(embed);
+  },
+};
