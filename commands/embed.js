105a81e9b741b7224e7f12f981c8aafdacd3506b
Arken05 <69765819+Arken05@users.noreply.github.com>
2021-08-15 22:28:18 +0100

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -10,7 +10,7 @@ module.exports = {
     const embed = new Discord.MessageEmbed()
       .setColor('#a822f8')
       .setTitle('**Settlement Classification Act 2021**')
-      .setDescription('All cities in the Federal Republic of Espaeria will now be subject to The Settlement Classification Act as detailed below. Each tier is graded upon its amount of residential buildings, services, institutions and attractions and can be re-assessed upon request')
+      .setDescription('All cities in the Federal Republic of Espaeria will now be subject to The Settlement Classification Act as detailed below. Each tier is graded upon its amount of residential buildings, services, institutions and attractions and can be re-assessed upon request.')
       .addFields(
         {name: '__Classifications__', value: ' insert detail ' },
       )
