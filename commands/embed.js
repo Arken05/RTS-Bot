f51aca99ffce67daf46afadcfa6fdf821ffc79f0
Arken05 <69765819+Arken05@users.noreply.github.com>
2021-08-20 19:24:58 +0100

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -12,7 +12,7 @@ module.exports = {
       .setTitle('**Settlement Classification Act 2021**')
       .setDescription('All cities in the Federal Republic of Espaeria will now be subject to The Settlement Classification Act as detailed below. Each tier is graded upon its amount of residential buildings, services, institutions and attractions and can be re-assessed upon request.')
       .addFields(
-        {name: '__Classifications__', value: ' Hamlet: \n\
+        {name: '__Classifications__', value: ' Hamlet: \n
 • A small collection of houses \n\
 • Little to no commercial services \n\
 
