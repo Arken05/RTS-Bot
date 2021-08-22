62c28a69431616777aff981f51646e26b2f55e48
Arken05 <69765819+Arken05@users.noreply.github.com>
2021-08-22 22:31:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,7 +40,7 @@ client.on('message', message => {
 		command.execute(message, args);
 	} catch (error) {
 		console.error(error);
-		message.reply('there was an error trying to execute that command!');
+		message.reply('there was an error trying to execut that command!');
 	}
 });
 
